d03c4b1c54a27732dec8bd9f6f58bef682338434
dmuruges <sekar.murugesan@gmail.com>
2020-08-14 23:13:45 +0530

    Added HomeController.cs  and Implemented Index() action method.

---
+++ EmployeeManagementSystem.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagementSystem.Web.Models;
+using EmployeeManagementSystem.Web.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+    }
+
+}
