948cba7271c179dadbc5cb54fd94b44f587ae5b5
dmuruges <sekar.murugesan@gmail.com>
2020-08-15 03:04:01 +0530

    Configured Attribute Routing only on Action Methods.

--- EmployeeManagementSystem.Web/Controllers/HomeController.cs
+++ EmployeeManagementSystem.Web/Controllers/HomeController.cs
@@ -17,18 +17,21 @@ namespace EmployeeManagementSystem.Web.Controllers
         {
             _employeeRepository = employeeRepository;
         }
-
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
         public ViewResult Index()
         {
             var model = _employeeRepository.GetAllEmployee();
             return View(model);
         }
 
-        public ViewResult Details(int id)
+        [Route("Home/Details/{id?}")]
+        public ViewResult Details(int? id)
         {
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                Employee = _employeeRepository.GetEmployee(1),
+                Employee = _employeeRepository.GetEmployee(id??1),
                 PageTitle = "Employee Details"
             };
 
--- EmployeeManagementSystem.Web/Startup.cs
+++ EmployeeManagementSystem.Web/Startup.cs
@@ -41,12 +41,13 @@ namespace EmployeeManagementSystem.Web
 
             app.UseStaticFiles();
             //app.UseMvcWithDefaultRoute();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+            app.UseMvc();
            
         }
     }
