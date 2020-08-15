809a87d9cdb91dde4418bbcaa87533fadf0de4c8
dmuruges <sekar.murugesan@gmail.com>
2020-08-15 19:08:35 +0530

    Added fictional company name "gravitas" to the default URL route. (Tag Helper)

--- EmployeeManagementSystem.Web/Startup.cs
+++ EmployeeManagementSystem.Web/Startup.cs
@@ -45,7 +45,7 @@ namespace EmployeeManagementSystem.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "gravitas/{controller=Home}/{action=Index}/{id?}");
             });
 
         }
